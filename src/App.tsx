fc696e392a555f0e8fb8aad545d6c38f6b1f9fe4
Takanobu <61861946+takanobu68@users.noreply.github.com>
2024-11-15 10:19:37 +0900

    tailwindCSS設定追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>テスト</h1>
+    <div className='flex items-center justify-center h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold text-blue-500'>テスト</h1>
     </div>
   );
 }
