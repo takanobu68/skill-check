113845b5a8aef9c79b191ac2a28206235f0dbb06
Takanobu <61861946+takanobu68@users.noreply.github.com>
2024-11-15 16:58:06 +0900

    選択された場所・年度・種類を管理するステートと更新関数を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import TitleSection from './components/sections/TitleSection';
@@ -6,6 +6,22 @@ import GraphArea from './components/sections/GraphArea';
 import SelectionPanel from './components/sections/SelectionPanel';
 
 const App = () => {
+  // 選択された場所・年度・種類を管理するステートを定義
+  const [selectedLocation, setSelectedLocation] = useState<string>('東京都');
+  const [selectedYear, setSelectedYear] = useState<number>(2018);
+  const [selectedType, setSelectedType] = useState<string>('住宅地');
+
+  // `SelectionPanel`コンポーネントがボタンクリック時に呼び出す関数
+  const handleSelectionSubmit = (
+    location: string,
+    year: number,
+    type: string
+  ) => {
+    setSelectedLocation(location);
+    setSelectedYear(year);
+    setSelectedType(type);
+  };
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
